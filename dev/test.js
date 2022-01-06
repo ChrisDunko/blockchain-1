1a89db165c4bad6c0a5c7f1e85752809152eb595
Christoph Dunkake <christoph@dragoneyes.org>
2022-01-06 21:25:37 +0100

    chain method

---
+++ dev/test.js
@@ -0,0 +1,8 @@
+const Blockchain = require('./blockchain');
+
+const bytecoin = new Blockchain();
+bytecoin.createNewBlock(2333, 'fhgjhghgjhbjhghfj', 'khkhkjhkjh');
+bytecoin.createNewBlock(345, 'joijlnlmlkmmi', 'klasdsdvfsdv');
+bytecoin.createNewBlock(2398, '5dfgdfgfbgb', '45345345fgfg');
+
+console.log(bytecoin);
